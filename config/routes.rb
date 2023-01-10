2240e903688f0026e7d22603dab7e2cf334ad438
octopus12176 <soutlook0103@gmail.com>
2023-01-11 00:11:03 +0900

    routes.rbにサンプル画面記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root  'users#index'  #この1行を追加
+
+  #ユーザー一覧を表示
+  get '/users', to: 'users#index'
+  #新規投稿（登録画面）に遷移
+  get '/users/new', to: 'users#new'
+  #投稿されたデータを受け取る
+  post '/users', to: 'users#create'
+
+  #投稿されてデータを編集する画面に行く
+  get '/users/:id/edit', to: 'users#edit'
+
+  #編集完了画面に遷移
+  patch 'users/:id', to: 'users#update'
+
+  # 投稿の削除を実施する
+  delete '/users/:id', to:'users#destroy'
 end
